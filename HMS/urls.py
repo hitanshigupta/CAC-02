5fd79509e1aa325e2ad590999d438d70ffb12de6
hitanshigupta <hitanshi.gupta@msds.christuniversity.in>
2024-01-08 14:05:04 +0530

    user template

--- HMS/urls.py
+++ HMS/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('cadmin.urls')),
-    path('users' , include('Users.urls')),
+    path('cadmin' , include('cadmin.urls')),
+    path('' , include('Users.urls')),
 ]
 
 if settings.DEBUG:
