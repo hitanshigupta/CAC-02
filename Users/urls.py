cf074d735e850671522ad0712a1e2d024059c342
devnaveenKrish <naveen.krishna@msds.christuniversity.in>
2024-01-08 13:40:55 +0530

    hw_list

--- Users/urls.py
+++ Users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Users' , views.index , name="index"),
+    path('' , views.index , name="index"),
     # path('URL' , views.DEF_NAME , NAME="VAR_NAME"),
 ]
