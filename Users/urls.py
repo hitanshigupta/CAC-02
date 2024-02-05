14a27a64ec1c008a56a63b0a68d43be61ec9a7e8
Aanand Kumar <aanandkumar@Aanands-MacBook-Air.local>
2024-02-05 22:38:19 +0530

    changed UI part

--- Users/urls.py
+++ Users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.index , name="index"),
+    path('', views.index , name="index"),
     path('user_login', views.user_login, name="user_login"),
     path('user_registration', views.user_registration, name="user_registration"),
     path('user_logout', views.user_logout, name="user_logout"),
